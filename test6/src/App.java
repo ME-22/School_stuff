d66baf17c8c423e84708f83fe9d5bd284beb4195
ME_22 <matias.isopahkala@gmail.com>
2025-10-24 00:40:34 +0300

    wow

--- test6/src/App.java
+++ test6/src/App.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        String[] colors = {};
+	public static void main(String[] args) {
+
+	    ArrayList<String> cars = new ArrayList<String>();
+	    cars.add("Kia");
+	    cars.add("Tesla");
+	    cars.add("BMW");
+	    cars.add("Renault");
+
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+        cars.add(1,"Ford");
+	    cars.remove(2);
+        cars.set(2,"Audi");
+        System.out.println("MODIFIED LIST");
+        for (String i : cars) {
+            System.out.println(i);
+	    }
+        cars.sort(null);
+        System.out.println("SORTED LIST");
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
     }
 }
